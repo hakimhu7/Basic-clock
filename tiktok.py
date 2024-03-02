154bcb4e91a843b8d6af98b7b75a9e8eec7f6a8b
hakimhu7 <159656433+hakimhu7@users.noreply.github.com>
2024-03-02 08:31:06 +0800

    Update tiktok.py
    Changed to CET timezone

--- tiktok.py
+++ tiktok.py
@@ -1,19 +1,19 @@
 from time import strftime #call time func
 from tkinter import *  #gui interface 
 from tkinter.ttk import * #gui theme
-import pytz
-from datetime import datetime
+import pytz #deals with timezone definitions
+from datetime import datetime  #deals with date and time manipulations
 table = Tk()  # creating window 
 table.title("Tikety-tokety") # naming the window 
 
 def time():
-    kazakhtime = pytz.timezone('Etc/GMT+6')
-    getkazakhtime = datetime.now(kazakhtime)
-    table =  getkazakhtime.strftime("%H:%M:%S %p" ) #strftime is used to format how the time will be displayed
+    CETtime = pytz.timezone('Europe/Brussels')
+    getCETtime = datetime.now(CETtime)
+    table =  getCETtime.strftime("%H:%M:%S %p") #strftime is used to format how the time will be displayed
     lbl.config(text=table) 
     lbl.after(1000, time)
 
-lbl = Label(table, font = ("gothic" , 40 ,"bold" ),
+lbl = Label(table, font = ("droid serif" , 40 ,"bold" ),
            background= 'dark blue', foreground= "white" )
 
 lbl.pack(anchor= "center") #must be n, ne, e, se, s, sw, w, nw, or center
